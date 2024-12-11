9e10b14c89a119478b26233a6c9aa316495075b5
bkojiro <153228944+bkojiro@users.noreply.github.com>
2024-12-11 11:13:58 -0800

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,20 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <iostream>
+#include "Student.h"
+
+using namespace std;
+
+class Node {
+ public:
+  Node(Student* stud); //create a node holding a student
+  ~Node(); //destructor, deletes memory and sets next to null
+  Node* getNext(); //gets the node in next
+  Student* getStudent(); //gets the student associated with the node
+  void setNext(Node* newNext); //allows more nodes to be attached next
+ private:
+  Node* next;
+  Student* student;
+};
+
